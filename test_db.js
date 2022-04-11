32da2aabd73780df3f499f77941fbb268932bd91
Chris Ross <chrisross00@gmail.com>
2022-04-11 17:45:25 -0600

    local "db" test files

---
+++ test_db.js
@@ -0,0 +1,40 @@
+stored_search_test = {
+    'searches': [{
+            'query': 'olivia',
+            'search_results': [{
+                'result_id': 'u0wzyl',
+                'body': {
+                    'author': 'shirouu-kun',
+                    'title': '[US-CA] [H] Built Think6.5 v2 Pink Assassin, Think6.5 v2 Solder PCB, 2U Badges, Alpaca and Silent Alpaca Switches, JTK Night Sakura, Latrialum [W] Paypal',
+                    'created date': '2022-04-10 20:13:00',
+                    'url': 'https://www.reddit.com/r/mechmarket/comments/u0wzyl/usca_h_built_think65_v2_pink_assassin_think65_v2/',
+                    'hash': '9278f1a8736b8f34f1cd26a36994a22cd527809a1bdd367f34482f69f32e4143'
+                }
+            }]
+        }, {
+            'query': '9009',
+            'search_results': [{
+                'result_id': 'u08jd9',
+                'body': {
+                    'author': 'TradersParad1se',
+                    'title': '[US-TX] [H] Keycult 2/65 Ocean Grey/ Silver, Tofu65 Grey Aluminum, Bauer 2 PC/Cyan, GMK Modo Light, GMK, GMK Modo2 [W] Paypal, GMK Redacted, GMK Dracula + Nightmode, GMK 9009',
+                    'created date': '2022-04-09 20:52:43',
+                    'url': 'https://www.reddit.com/r/mechmarket/comments/u08jd9/ustx_h_keycult_265_ocean_grey_silver_tofu65_grey/',
+                    'hash': 'f673a1d4ea5b049c407723788a3086235a7e5ebf3ca4d10fac0fb16e53d05312'
+                }
+            }]
+        }, {
+            'query': 'hammerhead',
+            'search_results': [{
+                'result_id': 'u0oded',
+                'body': {
+                    'author': 'WestC0ast_BestC0ast',
+                    'title': '[US-CA] [H] Below Cost Keyboards and GMK Keycaps [W] PayPal',
+                    'created date': '2022-04-10 12:59:16',
+                    'url': 'https://www.reddit.com/r/mechmarket/comments/u0oded/usca_h_below_cost_keyboards_and_gmk_keycaps_w/',
+                    'hash': 'c46e932a561f99d41129267fcd5ba7f5534a61846f7265dd967106ee44e721a3'
+                }
+            }]
+        }
+    ]
+}
