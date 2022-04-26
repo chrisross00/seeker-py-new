961682cb220f3dc8a284b0688165b1ca84fb77e0
Chris Ross <chrisross00@gmail.com>
2022-04-26 08:36:48 -0600

    Added flask-apscheduler for job running

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
+from runmain import runmain
 from flask import Flask, Blueprint
 from models.base import db, migrate
 from blueprints import query, message, search ,searchResultDb
+from flask_apscheduler import APScheduler
 
 BLUEPRINTS = [query, message, search ,searchResultDb]
 
@@ -12,10 +14,20 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS'] = False
-    
-    db.init_app(app)
-    migrate.init_app(app, db)
 
+    
+    #not sure if this should be on application.py or server.py...
+    scheduler = APScheduler()
+    scheduler.api_enabled = True
+
+    with app.app_context():
+        scheduler.init_app(app)
+        scheduler.start()
+        scheduler.add_job(id='job_1', func=runmain, args='', trigger='interval',seconds=20)
+        
+    db.init_app(app)
+    db.app = app
+    migrate.init_app(app, db)
     for blueprint in BLUEPRINTS:
         app.register_blueprint(blueprint)
 
